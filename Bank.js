9ec56be8dea4b7acdc1cda2489bcc340f7feef8f
Wajed Isleem <wajedbisleem@gmail.com>
2025-01-08 10:34:32 -0700

    Assignment Done

--- Bank.js
+++ Bank.js
@@ -7,7 +7,11 @@ class Bank {
 
     // Add methods here:
     // Example: createAccount(name, initialDeposit)
-
+    createAccount(name, initialDeposit) {
+        const account = new Account(name, initialDeposit);
+        this.accounts.push(account);
+        return account;
+    }
 }
 
 // Account Class: Represents a single user's account
@@ -21,16 +25,40 @@ class Account {
     // Add methods here:
     // Example: deposit(amount) 
     // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
-
+    deposit(amount) {
+        this.balance += amount;
+        this.transactionHistory.push({ transactionType: "Deposit", amount: amount });
+    }
+    
     // Example: withdraw(amount)
     // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
-
+    withdraw(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            this.transactionHistory.push({ transactionType: "Withdrawal", amount: amount });
+        } else {
+            console.log("Insufficient funds, You can't make this transaction");
+        }
+    }
     // Example: transfer(amount, recipientAccount)
     // example data to be stored in transactionHistory:
     // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
     // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
-    
+    transfer(amount, recipientAccount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            recipientAccount.balance += amount;
+            this.transactionHistory.push({ transactionType: "Transfer", amount: amount, to: recipientAccount.name });
+            recipientAccount.transactionHistory.push({ transactionType: "Received", amount: amount, from: this.name });
+        } else {
+            console.log("Insufficient funds, You can't make this transaction");
+        }
+    }  
+  
     // Example: checkBalance()
+    checkBalance() {
+        return this.balance;
+    }
 }
 
 //<-------------------------------DO NOT WRITE BELOW THIS LINE------------------------------>
